cdfad18c7e1e9b142eaf4b6f803803fa126daf29
Guilherme Fernandes Pereira <guilherme.pereira@vetron.com.br>
2018-12-12 17:39:25 -0200

    Added express and improved code

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
 const axios = require('axios');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  // res.end('Hello Node.JS!');
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('/binance', function (req, res) {
+  console.log(req, res)
 
   axios.get('https://api.binance.com/api/v1/trades?symbol=POLYBTC&limit=1')
   .then(function (response) {
-            res.end(JSON.stringify(response.data));
+    res.json(response.data);
   })
   .catch(function (error) {
-      console.log(error);
+    console.log(error);
   });
+})
 
-}).listen(8080);
-
-console.log('Server running at http://localhost:8080/');
+app.listen(port, function () {
+  console.log(`Listening on http://localhost:${port}`)
+})
